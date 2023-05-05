cdfc6acb1ee350358dcb034190e22e9b4e392280
Shweta Divakar Kulkarni <shweta.kulkarni3637@gmail.com>
2023-05-05 19:51:55 +0530

    Add files via upload

--- sentiment_analysis/home.py
+++ sentiment_analysis/home.py
@@ -24,6 +24,7 @@ decision_tree_clssifier=pickle.load(open(best_model, 'rb'))
 #Function to clean text
 import nltk
 nltk.download('stopwords')
+nltk.download('wordnet')
 from nltk.corpus import stopwords
 import re
 from nltk.stem import  PorterStemmer, WordNetLemmatizer
